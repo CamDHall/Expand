ef5670e3ba39f694de163512b24ac650977a9820
CamDHall <camerondylan.h@gmail.com>
2017-03-21 11:54:18 -0400

    Wall positions done.

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -6,11 +6,6 @@ public class Spawner : MonoBehaviour {
 
     // Wall
     public Walls walls;
-    public GameObject leftWall, rightWall;
-    float wallX = 0, wallY = 0;
-    public GameObject wallCell;
-    float cellHeight;
-    float leftCellX = 0, rightCellX = 0;
 
     // Goals
     public GameObject sphere;
@@ -31,14 +26,12 @@ public class Spawner : MonoBehaviour {
 	void Start () {
         obstacleTimer = Time.timeSinceLevelLoad + 1f;
         goalTimer = Time.timeSinceLevelLoad + 2f;
-        cellHeight = wallCell.GetComponent<SpriteRenderer>().sprite.bounds.size.y;
     }
 
     void Update() {
         if(obstacleTimer < Time.timeSinceLevelLoad)
         {
             walls.CreateWall();
-            // Obstacle();
             obstacleTimer = Time.timeSinceLevelLoad + Random.Range(lowLimit, highLimit);
         }
 
@@ -48,113 +41,6 @@ public class Spawner : MonoBehaviour {
             goalMultiplierTime = Time.timeSinceLevelLoad / Random.Range(50f, 60f);
             goalTimer = Time.timeSinceLevelLoad + Random.Range(goalTimerLow / goalMultiplierTime, goalTimerHigh / goalMultiplierTime);
         }
-    }
-
-    void Obstacle()
-    {
-        /*
-        // Parent Wall
-        GameObject Wall = new GameObject("Wall");
-        Wall.transform.position = new Vector3(0, 5, 0);
-        int wallHeight = Random.Range(5, 12);
-        float wallWidth = Random.Range(1.0f, 3.0f);
-        float bottomWidth = 0;
-
-        // Left Wall
-        GameObject leftWall = new GameObject("leftWall");
-        leftWall.transform.position = new Vector3((wallWidth/-2), Wall.transform.position.y, transform.position.z);
-
-        // Right Wall
-        GameObject rightWall = new GameObject("rightWall");
-        rightWall.transform.position = new Vector3((wallWidth / 2), Wall.transform.position.y, transform.position.z);
-
-        leftWall.transform.SetParent(Wall.transform);
-        rightWall.transform.SetParent(Wall.transform);
-
-        for(int i = 0; i < wallHeight; i++)
-        {
-            leftCellX = leftWall.transform.position.x;
-            rightCellX = rightWall.transform.position.x;
-            int moveLeft = Random.Range(0, 3);
-            if(moveLeft < 2)
-            {
-                int piecesPlaced = 0;
-                if (piecesPlaced < 1)
-                {
-                    leftCellX -= (cellHeight / 2);
-                    rightCellX -= (cellHeight / 2);
-                    piecesPlaced++;
-                } else if(piecesPlaced == 1)
-                {
-                    piecesPlaced++;
-                } else
-                {
-                    piecesPlaced = 0;
-                }
-            } else
-            {
-                int piecesPlaced = 0;
-                if (piecesPlaced < 1)
-                {
-                    leftCellX += (cellHeight/ 2);
-                    rightCellX += (cellHeight / 2);
-                    piecesPlaced++;
-                }
-                else if (piecesPlaced == 1)
-                {
-                    piecesPlaced++;
-                }
-                else
-                {
-                    piecesPlaced = 0;
-                }
-            }      
-
-            // Spawn Cells
-            Vector3 leftWallCellPos = new Vector3(leftCellX, leftWall.transform.position.y + (i * cellHeight), wallCell.transform.position.z);
-            Vector3 rightWallCellPos = new Vector3(rightCellX, rightWall.transform.position.y + (i * cellHeight), wallCell.transform.position.z);
-
-            // Left Cell
-            GameObject leftCell = Instantiate(wallCell);
-            leftCell.transform.SetParent(leftWall.transform);
-            leftCell.transform.position = leftWallCellPos;
-            leftCell.tag = "Wall";
-
-            // Right Cell
-            GameObject rightCell = Instantiate(wallCell);
-            rightCell.transform.SetParent(rightWall.transform);
-            rightCell.transform.position = rightWallCellPos;
-            rightCell.tag = "Wall";
-
-            // Get Extents
-            if(i == 0)
-            {
-                bottomWidth = rightCell.GetComponent<BoxCollider2D>().bounds.max.x - leftCell.GetComponent<BoxCollider2D>().bounds.min.x;
-
-            } 
-
-            // Add gravity
-            if (Wall.GetComponent<Rigidbody2D>() == null)
-            {
-                Wall.AddComponent<Rigidbody2D>();
-            }
-
-            // Gravity
-            lowGravityLimit = Mathf.Clamp((gravityModifier / Player.currentScale), 0.001f, 0.05f);
-            highGravityLimit = Mathf.Clamp((gravityModifier / Player.currentScale), 0.1f, .3f);
-            gravity = Random.Range(lowGravityLimit, highGravityLimit);
-
-            Wall.GetComponent<Rigidbody2D>().gravityScale = gravity;
-            // Create bounds and collider
-            if (Wall.GetComponent<BoxCollider2D>() == null)
-            {
-                // Bottom Trigger
-                Wall.AddComponent<BoxCollider2D>();
-                Wall.GetComponent<BoxCollider2D>().size = new Vector2(bottomWidth, 0.25f);
-                Wall.GetComponent<BoxCollider2D>().offset = new Vector2(0, 2);
-                Wall.GetComponent<BoxCollider2D>().isTrigger = true;
-            }
-        } */
     }
 
     void Goal()
--- Assets/Scripts/Walls.cs
+++ Assets/Scripts/Walls.cs
@@ -9,7 +9,7 @@ public class Walls : MonoBehaviour {
 
     public GameObject verticalWall, horizontalWall;
 
-    float wallHeight = 0;
+    float wallHeight = 0, wallWidth = 0;
     string lastPoint ="none";
     Vector3 lastPos;
     
@@ -21,16 +21,17 @@ public class Walls : MonoBehaviour {
     public virtual void CreateWall()
     {
         wallHeight = Random.Range(6, 20);
+        wallWidth = Random.Range(2.0f, 3.0f);
 
-        CreateLeftWall();
-
-        
-    }
-
-    void CreateLeftWall()
-    {
+        // Left Wall
         GameObject leftWall = new GameObject("leftWall");
         leftWall.transform.SetParent(Wall.transform);
+        leftWall.transform.position = new Vector3(-(wallWidth / 2), 3, 0);
+        lastPos = leftWall.transform.position;
+
+        // Right Wall
+        GameObject rightWall = new GameObject("rightWall");
+        rightWall.transform.SetParent(Wall.transform);
 
         // vars
         var _verticalYSize = verticalWall.GetComponent<SpriteRenderer>().bounds.size.y;
@@ -38,86 +39,107 @@ public class Walls : MonoBehaviour {
         var _horizontalXSize = horizontalWall.GetComponent<SpriteRenderer>().bounds.size.x;
         var _horizontalYSize = horizontalWall.GetComponent<SpriteRenderer>().bounds.size.y;
 
-        for(int i = 0; i < wallHeight; i++)
+        for (int i = 0; i < wallHeight; i++)
         {
-            Debug.Log(verticalWall.GetComponent<SpriteRenderer>().bounds.size.x);
             int choice = Random.Range(0, 6);
 
             // Vertical
             if (choice <= 2)
             {
-                var vertical = Instantiate(verticalWall);
+                var leftVerticalCell = Instantiate(verticalWall);
+                var rightVerticalCell = Instantiate(verticalWall);
 
                 // Position based on last piece position
                 if (lastPoint == "none")
                 {
-                    vertical.transform.position = new Vector3(0, 0);
-                } else if(lastPoint == "StayedVertical")
+                    leftVerticalCell.transform.position = lastPos;
+                }
+                else if (lastPoint == "StayedVertical")
                 {
-                    vertical.transform.position = new Vector3(lastPos.x, lastPos.y + _verticalYSize, 0);
-                } else if(lastPoint == "LeftHorizontal")
+                    leftVerticalCell.transform.position = new Vector3(lastPos.x, lastPos.y + _verticalYSize, 0);
+                }
+                else if (lastPoint == "LeftHorizontal")
                 {
-                    vertical.transform.position = new Vector3(lastPos.x - ((_horizontalXSize / 2) - (_verticalXSize / 2)), lastPos.y + ((_horizontalYSize / 2) + (_verticalYSize / 2)), 0);
-                } else if(lastPoint == "RightHorizontal")
+                    leftVerticalCell.transform.position = new Vector3(lastPos.x - ((_horizontalXSize / 2) - (_verticalXSize / 2)), lastPos.y + ((_horizontalYSize / 2) + (_verticalYSize / 2)), 0);
+                }
+                else if (lastPoint == "RightHorizontal")
                 {
-                    vertical.transform.position = new Vector3(lastPos.x + ((_horizontalXSize / 2) - (_verticalXSize / 2)), lastPos.y + ((_horizontalYSize / 2) + (_verticalYSize / 2)), 0);
+                    leftVerticalCell.transform.position = new Vector3(lastPos.x + ((_horizontalXSize / 2) - (_verticalXSize / 2)), lastPos.y + ((_horizontalYSize / 2) + (_verticalYSize / 2)), 0);
                 }
 
                 // Parenting, lastpoint
-                vertical.transform.SetParent(leftWall.transform);
-                lastPos = vertical.transform.position;
+                // Left
+                leftVerticalCell.transform.SetParent(leftWall.transform);
+                lastPos = leftVerticalCell.transform.position;
                 lastPoint = "StayedVertical";
+                // Right
+                rightVerticalCell.transform.position = new Vector3(leftVerticalCell.transform.position.x + (wallWidth), leftVerticalCell.transform.position.y, 0);
+                rightVerticalCell.transform.SetParent(rightWall.transform);
             }
 
             // Move left
-            else if(choice > 2 && choice <= 4)
+            else if (choice > 2 && choice <= 4)
             {
-                var leftHorizontal = Instantiate(horizontalWall);
-                leftHorizontal.transform.position = new Vector3(0, 0);
+                var leftHorizontalLeftCell = Instantiate(horizontalWall);
+                var leftHorizontalRightCell = Instantiate(horizontalWall);
 
                 // Position based on last piece position
                 if (lastPoint == "none")
                 {
-                    leftHorizontal.transform.position = new Vector3(0, 0);
-                } else if (lastPoint == "LeftHorizontal")
+                    leftHorizontalLeftCell.transform.position = lastPos;
+                }
+                else if (lastPoint == "LeftHorizontal")
                 {
-                    leftHorizontal.transform.position = new Vector3(lastPos.x - (_horizontalXSize), lastPos.y, 0);
-                } else if (lastPoint == "StayedVertical")
+                    leftHorizontalLeftCell.transform.position = new Vector3(lastPos.x - (_horizontalXSize), lastPos.y, 0);
+                }
+                else if (lastPoint == "StayedVertical")
                 {
-                    leftHorizontal.transform.position = new Vector3(lastPos.x - ((_horizontalXSize / 2) - (_verticalXSize / 2)), lastPos.y + ((_verticalYSize / 2) + (_horizontalYSize / 2)), 0);
-                } else if(lastPoint == "RightHorizontal")
+                    leftHorizontalLeftCell.transform.position = new Vector3(lastPos.x - ((_horizontalXSize / 2) - (_verticalXSize / 2)), lastPos.y + ((_verticalYSize / 2) + (_horizontalYSize / 2)), 0);
+                }
+                else if (lastPoint == "RightHorizontal")
                 {
-                    leftHorizontal.transform.position = new Vector3(lastPos.x, lastPos.y + (_horizontalYSize), 0);
+                    leftHorizontalLeftCell.transform.position = new Vector3(lastPos.x, lastPos.y + (_horizontalYSize), 0);
                 }
 
                 // Parenting, lastpoint
-                leftHorizontal.transform.SetParent(leftWall.transform);
-                lastPos = leftHorizontal.transform.position;
+                leftHorizontalLeftCell.transform.SetParent(leftWall.transform);
+                lastPos = leftHorizontalLeftCell.transform.position;
                 lastPoint = "LeftHorizontal";
+                // Right
+                leftHorizontalRightCell.transform.position = new Vector3(leftHorizontalLeftCell.transform.position.x + wallWidth, leftHorizontalLeftCell.transform.position.y, 0);
+                leftHorizontalRightCell.transform.SetParent(rightWall.transform);
             }
 
             // Move Right
             else
             {
-                var rightHorizontal = Instantiate(horizontalWall);
-                if(lastPoint == "none")
+                var rightHorizontalLeftCell = Instantiate(horizontalWall);
+                var rightHorizontalRightCell = Instantiate(horizontalWall);
+
+                if (lastPoint == "none")
                 {
-                    rightHorizontal.transform.position = new Vector3(0, 0);
-                } else if(lastPoint == "RightHorizontal")
+                    rightHorizontalLeftCell.transform.position = lastPos;
+                }
+                else if (lastPoint == "RightHorizontal")
                 {
-                    rightHorizontal.transform.position = new Vector3(lastPos.x + (_horizontalXSize), lastPos.y, 0);
-                } else if(lastPoint == "StayedVertical")
+                    rightHorizontalLeftCell.transform.position = new Vector3(lastPos.x + (_horizontalXSize), lastPos.y, 0);
+                }
+                else if (lastPoint == "StayedVertical")
                 {
-                    rightHorizontal.transform.position = new Vector3(lastPos.x + ((_horizontalXSize / 2) - (_verticalXSize/2)), lastPos.y + ((_verticalYSize / 2) + (_horizontalYSize / 2)), 0);
-                } else if(lastPoint == "LeftHorizontal")
+                    rightHorizontalLeftCell.transform.position = new Vector3(lastPos.x + ((_horizontalXSize / 2) - (_verticalXSize / 2)), lastPos.y + ((_verticalYSize / 2) + (_horizontalYSize / 2)), 0);
+                }
+                else if (lastPoint == "LeftHorizontal")
                 {
-                    rightHorizontal.transform.position = new Vector3(lastPos.x, lastPos.y + (_horizontalYSize), 0);
+                    rightHorizontalLeftCell.transform.position = new Vector3(lastPos.x, lastPos.y + (_horizontalYSize), 0);
                 }
 
                 // Parenting, lastpoint
-                rightHorizontal.transform.SetParent(leftWall.transform);
-                lastPos = rightHorizontal.transform.position;
+                rightHorizontalLeftCell.transform.SetParent(leftWall.transform);
+                lastPos = rightHorizontalLeftCell.transform.position;
                 lastPoint = "RightHorizontal";
+                // Right
+                rightHorizontalRightCell.transform.position = new Vector3(rightHorizontalLeftCell.transform.position.x + wallWidth, rightHorizontalLeftCell.transform.position.y, 0);
+                rightHorizontalRightCell.transform.SetParent(rightWall.transform);
             }
 
         }
