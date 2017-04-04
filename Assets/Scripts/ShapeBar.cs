9856e939c0a1162caeb3135c1dd750ac88e3bb5e
CamDHall <camerondylan.h@gmail.com>
2017-04-04 10:38:10 -0400

    Changed color pieces to match wall shapes.

---
+++ Assets/Scripts/ShapeBar.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShapeBar : MonoBehaviour {
+
+    public GameObject hex, square, triangle;
+
+	void Start () {
+		
+	}
+	
+	void Update () {
+		
+	}
+}
--- Assets/Scripts/Util/LevelManager.cs
+++ Assets/Scripts/Util/LevelManager.cs
@@ -6,13 +6,16 @@ using UnityEngine.SceneManagement;
 
 public class LevelManager : MonoBehaviour {
 
-
-    public static int level = 0;
+    public static int level = 10;
     public static Color levelColor;
 
     // Track and pick shape
     public static string lastShape;
     int shapeChoice;
+
+    // Level Grid
+    int width = 5;
+    int height = 5;
 
     // Image and fill
     public static float hexUIFill, squareUIFill, triangleUIFill;
@@ -23,7 +26,7 @@ public class LevelManager : MonoBehaviour {
     public Image _squareBackground, _squareFill, _hexBackground, _hexFill, _triangleBackground, _triangleFill;
     public static Vector2 hexagonUIPos, squareUIPos, triangleUIPos;
 
-	void Start () {
+    void Start() {
         hexagonUIPos = hexagonUI.transform.position;
         squareUIPos = squareUI.transform.position;
         triangleUIPos = triangleUI.transform.position;
@@ -31,41 +34,11 @@ public class LevelManager : MonoBehaviour {
         hexUIFill = 0;
         squareUIFill = 0;
         triangleUIFill = 0;
+    }
 
-        // pick color
-        levelColor = Random.ColorHSV();
-
-        // Create goal shapes
-        shapeChoice = Random.Range(1, 3);
-
-        if(shapeChoice == 1)
-        {
-            _hexBackground.gameObject.SetActive(true);
-            _hexFill.color = levelColor;
-        } else if(shapeChoice == 2)
-        {
-            _squareBackground.gameObject.SetActive(true);
-            _squareFill.color = levelColor;
-        } else
-        {
-            _triangleBackground.gameObject.SetActive(true);
-            _triangleFill.color = levelColor;
-        }
-	}
-	
-	void Update () {
+    void Update() {
         hexagonUI.GetComponent<Image>().fillAmount = hexUIFill;
         squareUI.GetComponent<Image>().fillAmount = squareUIFill;
         triangleUI.GetComponent<Image>().fillAmount = triangleUIFill;
-
-        _hexFill.GetComponent<Image>().fillAmount = levelFill;
-        _squareFill.GetComponent<Image>().fillAmount = levelFill;
-        _triangleFill.GetComponent<Image>().fillAmount = levelFill;
-
-        if(levelFill >= 1)
-        {
-            SceneManager.LoadScene("WinScreen");
-            level++;
-        }
     }
 }
